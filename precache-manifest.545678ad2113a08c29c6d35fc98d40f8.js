d071b7c2bd447dbb0f1b5abcb5dd401e143fb78f
Kyle <kzunino@gmail.com>
2019-07-23 13:36:39 -0700

    Updates

---
+++ precache-manifest.545678ad2113a08c29c6d35fc98d40f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2bb5da8646fc59f42f",
+    "url": "/course-directory/static/css/main.3fb943ec.chunk.css"
+  },
+  {
+    "revision": "ab2bb5da8646fc59f42f",
+    "url": "/course-directory/static/js/main.ab2bb5da.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "09351748e643fd02733093354f1e92f4",
+    "url": "/course-directory/index.html"
+  }
+];
